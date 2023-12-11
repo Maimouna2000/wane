fc49c778acaf655015d55354232c845b0f006134
maimouna <maifall26@gmail.com>
2023-12-11 10:53:24 +0100

    le projet

---
+++ laravel/database/migrations/2023_12_09_105939_create_membres_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membres', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+
+
+            $table->string('prenom');
+            $table->date('naissance');
+            $table->string('sexe');
+            
+            $table->foreign('categorie_id')->unsigned();
+            $table->foreign('categorie_id')->references('idc')->on('categories');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membres');
+    }
+};
